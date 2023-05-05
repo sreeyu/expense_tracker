d67a2947a7ca21347dfb4df2bcf0ee4c798093c1
sreey <sreelakshmi12898@gmail.com>
2023-05-05 12:15:12 +0530

    Add functionality to append new expense to existing list of expenses dynamically

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
+import React, { useState } from "react";
 import Expense from "./components/Expenses/Expense";
 import AddExpense from "./components/Expense Form/AddExpense";
 
 
+const expenseArray = [
+  {
+    id: 'e1',
+    title: 'Shopping',
+    amount: 450,
+    date: new Date(2023, 2, 8)
+  },
+  {
+    id: 'e2',
+    title: 'Food',
+    amount: 100,
+    date: new Date(2023, 0, 5)
+  },
+  {
+    id: 'e3',
+    title: 'Groceries',
+    amount: 621,
+    date: new Date(2022, 6, 15)
+  },
+  {
+    id: 'e4',
+    title: 'Books',
+    amount: 399,
+    date: new Date(2021, 5, 26)
+  }
+];
+
 function App() {
 
-  const expenseArray = [
-    {
-      id: 'e1',
-      title: 'Shopping',
-      amount: 450,
-      date: new Date(2023, 2, 8)
-    },
-    {
-      id: 'e2',
-      title: 'Food',
-      amount: 100,
-      date: new Date(2023, 0, 5)
-    },
-    {
-      id: 'e3',
-      title: 'Groceries',
-      amount: 621,
-      date: new Date(2022, 6, 15)
-    },
-    {
-      id: 'e4',
-      title: 'Books',
-      amount: 399,
-      date: new Date(2021, 5, 26)
-    }
-  ];
+  const [newExpenses, setNewExpenses] = useState(expenseArray)
 
   const getNewExpense = (newExpenseData) =>{
-    console.log(newExpenseData);
+    setNewExpenses((prevExpense) =>{
+      return [newExpenseData, ...prevExpense];
+    })
   }
   
   return (
     <div className="App">
       <AddExpense onEnterExpense={getNewExpense} />
-      <Expense items={expenseArray} />
+      <Expense items={newExpenses} />
     </div>
   );
 }
